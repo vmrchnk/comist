128d3c431e07883e028256e311d5589f314ecac8
lilcappucc <lilcappucc@gmail.com>
2019-10-29 19:39:09 +0200

    Initial Commit

---
+++ Comist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Comist
+//
+//  Created by dewill on 29.10.2019.
+//  Copyright © 2019 lilcappucc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
